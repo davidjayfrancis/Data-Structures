c4e61e30bd90c44faf18ffe7350473bee618e4b6
David Francis <davidjayfrancis@gmail.com>
2020-07-30 21:22:12 -0700

    added a __str__ method to singly_linked_list that prints out the nodes in order and added a 'remove_tail' method

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -19,6 +19,18 @@ class LinkedList:
         self.tail = None
         self.length = 0
 
+    def __str__(self):
+        cur_node = self.head
+        x = '['
+        while cur_node is not None:
+            if cur_node.get_next() is None:
+                x = x + str(cur_node.value) + ']'
+                cur_node = cur_node.get_next()
+                return x
+            x = x + str(cur_node.value) + ', '
+            cur_node = cur_node.get_next()
+        return x
+
     def add_to_head(self, value):
         new_node = Node(value, self.head)
         self.head = new_node
@@ -63,6 +75,24 @@ class LinkedList:
             self.head = temp
             self.length -=1
             return old_head.value
+
+    def remove_tail(self):
+        if self.length == 0:
+            return None
+        elif self.length == 1: 
+            x = self.tail
+            self.head = None
+            self.tail = None
+            return x
+        else: 
+            # find node that points to self.tail
+            cur_node = self.head
+            while cur_node.get_next() is not self.tail:
+                cur_node = cur_node.get_next()
+            cur_node.set_next(None)
+            x = self.tail
+            self.tail = cur_node
+            return x
         
     def get_max(self):
         cur_node = self.head
@@ -78,3 +108,14 @@ class LinkedList:
         return max_val
 
 
+l = LinkedList()
+l.add_to_tail(1)
+l.add_to_tail(1)
+l.add_to_tail(1)
+l.add_to_tail(4)
+
+print(l)
+
+l.remove_tail()
+
+print(l)
